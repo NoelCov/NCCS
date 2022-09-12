9bd0e749d263f5aa0c6f3a326a5508bffea09792
Noel <no3lcodes@gmail.com>
2022-09-11 18:49:31 -0700

    Added a form to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,81 @@
 import React from "react";
 
+import style from "../styles/contact.module.scss";
+
+import { Layout } from "../components/layout/layout.component";
+
 export default function Contact() {
-    return <div>Get in contact with us page</div>;
+    return (
+        <div className={style.contactPage}>
+            <Layout>
+                <form className={style.formContainer} autoComplete="off">
+                    <div className={style.namesInputContainer}>
+                        <div>
+                            <label htmlFor="firstName" className={style.label}>
+                                First name <span>{"  *"}</span>
+                            </label>
+                            <input
+                                id="firstName"
+                                name="firstName"
+                                type="text"
+                                className={style.inputField}
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="lastName" className={style.label}>
+                                Last name <span>{"  *"}</span>
+                            </label>
+                            <input
+                                id="lastName"
+                                name="lastName"
+                                type="text"
+                                className={style.inputField}
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="email" className={style.label}>
+                                Email <span>{"  *"}</span>
+                            </label>
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                className={style.inputField}
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="subject" className={style.label}>
+                                Subject <span>{"  *"}</span>
+                            </label>
+                            <input
+                                id="subject"
+                                name="subject"
+                                type="text"
+                                className={style.inputField}
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="message" className={style.label}>
+                                Message <span>{"  *"}</span>
+                            </label>
+                            <textarea
+                                id="message"
+                                name="message"
+                                className={style.inputField}
+                                required
+                            />
+                        </div>
+                    </div>
+                </form>
+            </Layout>
+        </div>
+    );
 }
